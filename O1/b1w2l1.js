89f3b957cdc9e5fbe485e0bddd33b29b9738b29b
99051717 <99051717@mydavinci.nl>
2020-09-07 12:27:50 +0200

    push

---
+++ O1/b1w2l1.js
@@ -0,0 +1,7 @@
+var voornaam = prompt("voornaam")
+var achternaam = prompt("achternaam")
+var leeftijd = prompt("leeftijd");
+
+document.write(voornaam + "<br>")
+document.write(achternaam + "<br>")
+document.write(leeftijd + "<br>")
